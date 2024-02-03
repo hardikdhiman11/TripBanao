127f731b24c2b68523d71a7057b3eb34cf4768b8
NavikaranDev <hardik11dhiman@gmail.com>
2024-02-03 13:51:35 +0530

    initial commit

---
+++ src/main/java/com/example/TripBanao/TripBanaoApplication.java
@@ -0,0 +1,15 @@
+package com.example.TripBanao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class
+
+TripBanaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripBanaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TripBanao/TripBanaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TripBanao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripBanaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
