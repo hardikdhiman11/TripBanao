fa9482b4837a457cb50214de8d79486c9afb022f
NavikaranDev <hardik11dhiman@gmail.com>
2024-02-03 14:28:48 +0530

    Initial commit

--- src/main/java/com/example/TripBanao/TripBanaoApplication.java
+++ src/main/java/com/example/TripBanao/TripBanaoApplication.java
@@ -4,9 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class
-
-TripBanaoApplication {
+public class TripBanaoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TripBanaoApplication.class, args);
